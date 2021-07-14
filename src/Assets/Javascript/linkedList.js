d3875000c3b1a6d2066769ea826242935de99d11
Mario Chois <mario.chois.navarro@gmail.com>
2021-07-13 22:59:15 -0500

    Added html constructor function

--- src/Assets/Javascript/linkedList.js
+++ src/Assets/Javascript/linkedList.js
@@ -4,82 +4,116 @@ class newNode{
       this.nextNode = nextNode;
     }
   }
-  
+
 export default class linkedList{
-    add(number) {
+  add(number) {
+    if(this.head){
+      let newItem = new newNode(number,this.head)
+      this.head = newItem
+    }else{
+      let newItem = new newNode(number)
+      this.head = newItem
+    }
+  }
+  
+  pop() {
+    if(this.head){
+      if(this.head){
+        let itemValue = this.head
+        this.head = this.head.nextNode
+        return itemValue.value
+      }else{
+        let itemValue = this.head
+        this.head = null;
+        return itemValue.value
+      }
+    }else{
+      return false
+    }
+  }
+
+  returnArray(){
+    if(!this.head){
+      return false
+    }else{
+      let allArray = []
+      let currentNode = this.head
+      allArray.push(currentNode.value)
+      while(currentNode.nextNode){
+        currentNode = currentNode.nextNode;
+        allArray.push(currentNode.value)
+      }
+      return allArray
+    }
+  }
+
+    remove(index=null){
+
+    if(index){
         if(this.head){
-          let newItem = new newNode(number,this.head)
-          this.head = newItem
-        }else{
-          let newItem = new newNode(number)
-          this.head = newItem
-        }
-      }
-      
-      pop() {
-        if(this.head){
-          if(this.head){
-            let itemValue = this.head
-            this.head = this.head.nextNode
-            return itemValue.value
-          }else{
-            let itemValue = this.head
-            this.head = null;
-            return itemValue.value
-          }
-        }else{
-          return "No elements Inside"
-        }
-      }
-    
-      showElems(){
-        if(!this.head){
-          return "no elements"
-        }else{
-          let allArray = []
-          let currentNode = this.head
-          allArray.push(currentNode.value)
-          while(currentNode.nextNode){
-            currentNode = currentNode.nextNode;
-            allArray.push(currentNode.value)
-          }
-          return allArray
-        }
-        
-        
-      }
-    
-      remove(){
-        if(!this.head){
-          return -1
-        }else{
             let valArray = []
             let activeValue = this.head
-            
             valArray.push(activeValue.value)
-            while(activeValue.nextNode){
-              activeValue = activeValue.nextNode
-              valArray.push(activeValue.value)
+                while(activeValue.nextNode){
+                    activeValue = activeValue.nextNode
+                    valArray.push(activeValue.value)
+                }
+                if(index>valArray.length){
+                return false
+                }else{
+                    let newArray = []
+                    for(let i = 0;i<valArray.length;i+=1){
+                        if(i != index){
+                        newArray.push(valArray[i])
+                        }
+                    }
+                    let returnValue = valArray[index]
+                    valArray = newArray.reverse()
+                    if(newArray.length !== 0){
+                        let onHold = new newNode(valArray[0])
+                        for(let i = 1;i<valArray.length;i += 1){ 
+                            onHold = new newNode(valArray[i],onHold)
+                        }
+                        this.head = onHold
+                    }else{
+                        this.head = null
+                    }
+                return returnValue
+                }
+            }else{
+                    return false
+                    }
+    }else{
+            if(!this.head){
+                return -1
+            }else{
+                let valArray = []
+                let activeValue = this.head
+                
+                valArray.push(activeValue.value)
+                while(activeValue.nextNode){
+                    activeValue = activeValue.nextNode
+                    valArray.push(activeValue.value)
+                }
+                let subsArray = []
+                for(let i =0;i<valArray.length;i+=1){
+                    if(i != valArray.length-1){
+                    subsArray.push(valArray[i])
+                    }
+                }
+                let returnValue = valArray[valArray.length-1]
+                valArray = subsArray.reverse()
+                    if(subsArray.length !== 0){
+                    let onHold = new newNode(valArray[0])
+                    for(let i = 1;i<valArray.length;i += 1){ 
+                        onHold = new newNode(valArray[i],onHold)
+                    }
+                    this.head = onHold
+                    }else{
+                    this.head = null
+                    }
+                return returnValue  
             }
-            let subsArray = []
-            for(let i =0;i<valArray.length;i+=1){
-              if(i != valArray.length-1){
-                subsArray.push(valArray[i])
-              }
-            }
-            let returnValue = valArray[valArray.length-1]
-            valArray = subsArray.reverse()
-              if(subsArray.length !== 0){
-                let onHold = new newNode(valArray[0])
-              for(let i = 1;i<valArray.length;i += 1){ 
-                  onHold = new newNode(valArray[i],onHold)
-              }
-              this.head = onHold
-              }else{
-                this.head = null
-              }
-            return returnValue
-          
         }
-      }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import htmlBuilder from './Assets/Javascript/modules.js';
+import linkedList from './Assets/Javascript/linkedList.js';
 import './style.css';
+import { create } from 'lodash';
 
 const toDoList = document.getElementById('to_do_list')
-let newTaskInput = document.createElement('newTaskInput')
-let taskList = []
+let newTaskInput = document.getElementById('newTaskInput')
+let taskList = new linkedList();
 
 
 
 function build(){
-    let itemsArray = []
-    
-
-
-
-
-    htmlBuilder(itemsArray)
+    let objectsArray = []
+    let itemsArray = taskList.returnArray();
+    for(let i = 0;i<itemsArray.length;i+=1){
+        let newTaskObj = document.createElement('li')
+        objectsArray.push([toDoList,newTaskObj,'taskItem',`${itemsArray[i]}`])
+    }
+    htmlBuilder(objectsArray)
 }
-
-
 
 newTaskInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
+        taskList.add(newTaskInput.value)
       build()
     }
 });
