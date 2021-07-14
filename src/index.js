492c0a7aed404a8a8674dee2e6c53ea903c8b43e
Mario Chois <mario.chois.navarro@gmail.com>
2021-07-13 21:03:49 -0500

    Iniciated webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import htmlBuilder from './Assets/Javascript/modules.js';
+import './style.css';
+
+
+
+
+
+
+  let itemsArray = []
+  let newComponent = document.createElement('h1')
+  itemsArray.push([bodyObj,newComponent,null,"guacamole"])
+  htmlBuilder(itemsArray)
+
+  
